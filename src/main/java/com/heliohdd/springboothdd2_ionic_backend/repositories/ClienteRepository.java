ba014c9671c3a32bcdb253e5732d8c913b039412
heliohdd <heliohdd@yahoo.com.br>
2020-03-19 15:35:18 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/heliohdd/springboothdd2_ionic_backend/repositories/ClienteRepository.java
+++ src/main/java/com/heliohdd/springboothdd2_ionic_backend/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.heliohdd.springboothdd2_ionic_backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.heliohdd.springboothdd2_ionic_backend.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/heliohdd/springboothdd2_ionic_backend/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/heliohdd/springboothdd2_ionic_backend/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.heliohdd.springboothdd2_ionic_backend.domain.Cliente;
 import com.heliohdd.springboothdd2_ionic_backend.domain.enums.TipoCliente;
 import com.heliohdd.springboothdd2_ionic_backend.dto.ClienteNewDTO;
+import com.heliohdd.springboothdd2_ionic_backend.repositories.ClienteRepository;
 import com.heliohdd.springboothdd2_ionic_backend.resources.exception.FieldMessage;
 import com.heliohdd.springboothdd2_ionic_backend.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -25,6 +32,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
