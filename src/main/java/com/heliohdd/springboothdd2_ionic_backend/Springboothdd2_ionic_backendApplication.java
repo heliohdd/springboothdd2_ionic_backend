d2bfddbbc158e3af5acc6da7bc2ba04957787abd
heliohdd <heliohdd@yahoo.com.br>
2020-03-25 10:34:21 -0300

    Padronizando formato de excecoes

--- src/main/java/com/heliohdd/springboothdd2_ionic_backend/Springboothdd2_ionic_backendApplication.java
+++ src/main/java/com/heliohdd/springboothdd2_ionic_backend/Springboothdd2_ionic_backendApplication.java
@@ -1,11 +1,8 @@
 package com.heliohdd.springboothdd2_ionic_backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.heliohdd.springboothdd2_ionic_backend.services.S3Service;
 
 @SpringBootApplication
 public class Springboothdd2_ionic_backendApplication implements CommandLineRunner {
