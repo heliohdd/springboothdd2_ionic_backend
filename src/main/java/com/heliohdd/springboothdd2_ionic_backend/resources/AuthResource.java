04f7978f001e6bc94acca669b1c4809d7e3cb18b
heliohdd <heliohdd@yahoo.com.br>
2020-03-27 15:21:24 -0300

    Esqueci a senha

--- src/main/java/com/heliohdd/springboothdd2_ionic_backend/resources/AuthResource.java
+++ src/main/java/com/heliohdd/springboothdd2_ionic_backend/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.heliohdd.springboothdd2_ionic_backend.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.heliohdd.springboothdd2_ionic_backend.dto.EmailDTO;
 import com.heliohdd.springboothdd2_ionic_backend.security.JWTUtil;
 import com.heliohdd.springboothdd2_ionic_backend.security.UserSS;
+import com.heliohdd.springboothdd2_ionic_backend.services.AuthService;
 import com.heliohdd.springboothdd2_ionic_backend.services.UserService;
 
 @RestController
@@ -18,6 +22,9 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -25,6 +32,12 @@ public class AuthResource {
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
 		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/heliohdd/springboothdd2_ionic_backend/services/EmailService.java
+++ src/main/java/com/heliohdd/springboothdd2_ionic_backend/services/EmailService.java
@@ -1,8 +1,10 @@
 package com.heliohdd.springboothdd2_ionic_backend.services;
 
 import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
+import com.heliohdd.springboothdd2_ionic_backend.domain.Cliente;
 import com.heliohdd.springboothdd2_ionic_backend.domain.Pedido;
 
 public interface EmailService {
@@ -14,4 +16,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
